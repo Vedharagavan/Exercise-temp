222ce9280213c1621fd0436bcfed9cb5eb4c383a
vedharagavan <vedharagavan7@gmail.com>
2019-01-23 11:30:49 +0530

    IP method added

--- init_gui.py
+++ init_gui.py
@@ -4,28 +4,75 @@ import sys
 from ruamel.yaml import YAML
 
 
-def combo_action(event):
-    global e_password
-    if card_name.get() == 'OPEN':
-        l_password.config(text='')
-        e_password.destroy()
+class Label_entry_box:
+    def __init__(self, l_text, row_no):
+        self.label = Label(window, text=l_text, fg='azure', bg='dim gray')
+        self.label.grid(row=row_no, sticky=E, padx=10, pady=10)
+        self.ebox = Entry(window, bg='snow')
+        self.ebox.grid(row=row_no, column=1, padx=10, pady=10)
+
+
+class Tk_var:
+    def __init__(self):
+        self.label = Label(window, text=' ', fg='azure', bg='dim grey')
+        self.ebox = Entry(window, bg='snow')
+
+    def labl(self, l_text, row_no):
+        self.label = Label(window, text=l_text, fg='azure', bg='dim grey')
+        self.label.grid(row=row_no, sticky=E, padx=10, pady=10)
+
+    def ebox(self, row_no):
+        self.ebox = Entry(window, bg='snow')
+        self.ebox.grid(row=row_no, column=1, padx=10, pady=10)
+
+
+def cmd_encrypt(event):
+    global password
+    if encrypt_type.get() == 'OPEN':
+        password.label.config(text='')
+        password.ebox.destroy()
     else:
-        l_password.config(text='password : ')
-        e_password = Entry(window, bg='snow')
-        e_password.grid(row=4, column=1, padx=10, pady=10)
+        password = Label_entry_box('Password  :  ', 4)
+
+
+def cmd_ip_method(event):
+    global ip_prefix, netmask, def_gw
+    if ip_method.get() == 'STATIC-IP':
+        ip_prefix = Label_entry_box('IP Prefix', 6)
+        netmask = Label_entry_box('Netmask', 7)
+        def_gw = Label_entry_box('Default Gateway', 8)
+        btn.grid(row=9, column=1, padx=10, pady=10)
+    else:
+        ip_prefix.label.destroy()
+        ip_prefix.ebox.destroy()
+        netmask.label.destroy()
+        netmask.ebox.destroy()
+        def_gw.label.destroy()
+        def_gw.ebox.destroy()
+        btn.grid(row=6, column=1, padx=10, pady=10)
 
 
 def get_details():
     file_name = 'conf_gui.yaml'
     with open(file_name) as fp:
         file_load = YAML().load(fp)
-    file_load['Group 1']['No_of_clients'] = e_no_of_clients.get()
+    file_load['Group 1']['No_of_clients'] = int(no_of_clients.ebox.get())
     file_load['Group 1']['Connection'] = 'SSID-1'
-    file_load['SSID-1']['SSID'] = e_ssid.get()
-    if e_password.get().lower() == 'none':
-        file_load['SSID-1']['key_mgmt'] = e_password.get()
+    file_load['SSID-1']['SSID'] = ssid.ebox.get()
+    if encrypt_type.get().lower() == 'open':
+        file_load['SSID-1']['key_mgmt'] = 'none'
     else:
-        file_load['SSID-1']['password'] = e_password.get()
+        file_load['SSID-1']['key_mgmt'] = 'WPA-PSK'
+        file_load['SSID-1']['password'] = password.ebox.get()
+    if ip_method.get().lower() == 'dhcp':
+        file_load['Group 1']['Static_ip'] = 'no'
+        file_load['Group 1']['dhcp_ip'] = 'yes'
+    else:
+        file_load['Group 1']['Static_ip'] = 'yes'
+        file_load['Group 1']['ip_prefix'] = ip_prefix.ebox.get()
+        file_load['Group 1']['netmask'] = netmask.ebox.get()
+        file_load['Group 1']['default_gateway'] = def_gw.ebox.get()
+        file_load['Group 1']['dhcp_ip'] = 'no'
     with open(file_name, 'w')as fp:
         YAML().dump(file_load, fp)
     window.destroy()
@@ -33,35 +80,31 @@ def get_details():
 
 window = Tk()
 window.config(bg='dim gray')
-window.wm_geometry("500x300+300+100")
+window.wm_geometry("500x450+300+150")
 window.title("Virtual WiFi Clients")
 
-l_no_of_clients = Label(window, text='              ', fg='azure', bg='dim gray')
-l_no_of_clients.grid(row=0, sticky=E)
-l_no_of_clients = Label(window, text='No of Clients : ', fg='azure', bg='dim gray')
-l_no_of_clients.grid(row=1, sticky=E, padx=10, pady=10)
-l_ssid = Label(window, text='SSID : ', fg='azure', bg='dim gray')
-l_ssid.grid(row=2, sticky=E, padx=10, pady=10)
-l_encryption = Label(window, text='WiFi Encryption : ', fg='azure', bg='dim gray')
-l_encryption.grid(row=3, sticky=E, padx=10, pady=10)
-l_password = Label(window, text='Password : ', fg='azure', bg='dim gray')
-l_password.grid(row=4, sticky=E, padx=10, pady=10)
+Tk_var().labl('              ', 0)
+no_of_clients = Label_entry_box('No of Clients  :  ', 1)
+ssid = Label_entry_box('SSID  :  ', 2)
+Tk_var().labl('WiFi Encryption  :  ', 3)
+password = Label_entry_box('Password  :  ', 4)
+Tk_var().labl('IP Method  :  ', 5)
 
-card_name = StringVar()
-combo_encrypt = ttk.Combobox(window, width=8, textvariable=card_name,
+encrypt_type = StringVar()
+combo_encrypt = ttk.Combobox(window, width=8, textvariable=encrypt_type,
                              state='readonly', values=['WPA-PSK', 'OPEN'])
 combo_encrypt.grid(row=3, column=1, padx=10, pady=10)
-combo_encrypt.bind("<<ComboboxSelected>>", combo_action)
+combo_encrypt.current(0)
+combo_encrypt.bind("<<ComboboxSelected>>", cmd_encrypt)
 
-e_no_of_clients = Entry(window, bg='snow')
-e_no_of_clients.grid(row=1, column=1, padx=10, pady=10)
-e_ssid = Entry(window, bg='snow')
-e_ssid.grid(row=2, column=1, padx=10, pady=10)
-e_password = Entry(window, bg='snow')
-e_password.grid(row=4, column=1, padx=10, pady=10)
-
+ip_method = StringVar()
+combo_ip_method = ttk.Combobox(window, width=8, textvariable=ip_method,
+                               state='readonly', values=['DHCP', 'STATIC-IP'])
+combo_ip_method.grid(row=5, column=1, padx=10, pady=10)
+combo_ip_method.current(0)
+combo_ip_method.bind("<<ComboboxSelected>>", cmd_ip_method)
 
 btn = Button(window, text='Submit', bg='SkyBlue', command=get_details)
-btn.grid(row=5, column=1, padx=10, pady=10)
+btn.grid(row=6, column=1, padx=10, pady=10)
 
 window.mainloop()
